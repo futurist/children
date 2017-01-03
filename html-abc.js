d5ce07cfa25935edcce62ebb6b99a2ef37885d54
futurist <jamesyang999@gmail.com>
2017-01-03 22:29:42 +0800

    add: key code and next interval

--- html-abc.js
+++ html-abc.js
@@ -1,20 +1,26 @@
+var cur = 0
 var abc = document.getElementById('abc')
-abc.play()
 
 window.onkeydown = e => {
-  console.log(e)
-  if (e.code = 'Space') {
+  // quit if video is playing, or out of seq
+  if (!abc.paused || cur >= seq.length) return
+  // e.code: Space, KeyA, KeyB...
+  if (e.code === seq[cur].key) {
     // togglePlay(abc)
+    abc.play()
+    setTimeout(
+      () => abc.pause(),
+      seq[cur].next
+    )
+    cur++
   }
 }
 
-abc.addEventListener(
-  'timeupdate',
-  function (e) {
-    time.innerHTML = abc.currentTime
-  },
-  true
-)
+const seq = [
+  {key: 'KeyA', next: 2000},
+  {key: 'KeyB', next: 2000},
+  {key: 'KeyC', next: 2000}
+]
 
 function togglePlay (video) {
   if (video.paused) {
